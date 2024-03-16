20ec0f4df26f8a61f023c6960f6c98a8a3187bc6
lsh <59404696+peterroe@users.noreply.github.com>
2024-03-16 16:32:14 +0800

    chore: update readme

--- src/material/index.ts
+++ src/material/index.ts
@@ -1,4 +1,5 @@
-import { fetch } from 'ofetch'
+import { resolve } from 'path'
+import { readFileSync } from 'fs'
 import { fileIcons } from '../../vendor/vscode-material-icon-theme/src/icons/fileIcons'
 import { folderIcons } from '../../vendor/vscode-material-icon-theme/src/icons/folderIcons'
 import { languageIcons } from '../../vendor/vscode-material-icon-theme/src/icons/languageIcons'
@@ -7,7 +8,9 @@ import { mergeInner } from './util'
 export const defaultFolderIcon = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="#8699a3" d="M4 20q-.825 0-1.412-.587T2 18V6q0-.825.588-1.412T4 4h6l2 2h8q.825 0 1.413.588T22 8v10q0 .825-.587 1.413T20 20z"/></svg>'
 export const defaultFileIcon = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><path fill="#8699a3" d="m210.83 85.17l-56-56A4 4 0 0 0 152 28H56a12 12 0 0 0-12 12v176a12 12 0 0 0 12 12h144a12 12 0 0 0 12-12V88a4 4 0 0 0-1.17-2.83M156 41.65L198.34 84H156ZM200 220H56a4 4 0 0 1-4-4V40a4 4 0 0 1 4-4h92v52a4 4 0 0 0 4 4h52v124a4 4 0 0 1-4 4"/></svg>'
 
-export async function getIconByFile(fileName: string, isUseDefault = true) {
+const staticPath = process.env.NODE_ENV === 'production' ? resolve(__dirname, './material') : resolve(__dirname, '../../vendor/vscode-material-icon-theme/icons')
+
+export function getIconByFile(fileName: string, isUseDefault = true) {
   let iconName = fileIcons.icons.find((item) => {
     if (item.fileNames?.includes(fileName))
       return true
@@ -33,19 +36,11 @@ export async function getIconByFile(fileName: string, isUseDefault = true) {
   if (iconName === 'file')
     return defaultFileIcon
 
-  try {
-    const data = fetch(`https://cdn.jsdelivr.net/gh/PKief/vscode-material-icon-theme@master/icons/${iconName}.svg`)
-      .then(data => data.text())
-
-    return data
-  }
-  catch (e) {
-    console.error(e)
-    return ''
-  }
+  const data = readFileSync(`${staticPath}/${iconName}.svg`, 'utf-8')
+  return data
 }
 
-export async function getIconByFolder(fileName: string, isUseDefault = true) {
+export function getIconByFolder(fileName: string, isUseDefault = true) {
   const specific = folderIcons.find(item => item.name === 'specific')
   if (!specific)
     return ''
@@ -65,14 +60,7 @@ export async function getIconByFolder(fileName: string, isUseDefault = true) {
   if (iconName === 'folder')
     return defaultFolderIcon
 
-  try {
-    const data = fetch(`https://cdn.jsdelivr.net/gh/PKief/vscode-material-icon-theme@master/icons/${iconName}.svg`)
-      .then(data => data.text())
+  const data = readFileSync(`${staticPath}/${iconName}.svg`, 'utf-8')
 
-    return data
-  }
-  catch (e) {
-    console.error(e)
-    return ''
-  }
+  return data
 }
--- tsup.config.ts
+++ tsup.config.ts
@@ -1,12 +1,26 @@
 import { builtinModules } from 'module'
 import { defineConfig } from 'tsup'
+import { copy } from 'esbuild-plugin-copy'
 
 import pkg from './package.json'
 
 export default defineConfig({
   entry: {
     material: 'src/material/index.ts',
+    index: 'src/index.ts',
   },
+  esbuildPlugins: [
+    copy({
+      resolveFrom: 'cwd',
+      assets: [
+        {
+          from: './vendor/vscode-material-icon-theme/icons/*',
+          to: './dist/material',
+        },
+      ],
+    }),
+  ],
+  shims: true,
   clean: true,
   minify: true,
   format: ['esm', 'cjs'],
